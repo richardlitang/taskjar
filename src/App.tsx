27357c973a5062f56525a28f24822ede4fb3bed7
Richard <richardjasonlitang@gmail.com>
2022-02-17 09:39:21 +0800

    added basic frontend func

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [task, setTask] = useState<Task>()
+
+  interface Task {
+    name: string,
+    description?: string,
+    duration?: number,
+    upvotes?: number
+  }
+
+  const fetchTask = async () => {
+    await fetch('http://localhost:5000/api/tasks').
+      then(response => response.json()).
+      then(data => setTask(data[0]))
+  } 
+
+  useEffect(() => {
+    fetchTask().catch(console.error)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <div className="Logo-container">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
+      <div className="task">
+        <span className="task_name">{task?.name}</span>
+        <span className="task_description">{task?.description}</span>
+        <button className="task_search" onClick = {fetchTask}>
+          Find a task
+        </button>
+      </div>
     </div>
   );
 }
