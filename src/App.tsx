2b16cf33dbd0f8dfeb991ffeffceb2566d22b317
Richard <richardjasonlitang@gmail.com>
2022-02-19 14:22:18 +0800

    improved design for home dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Tasks from './pages/Tasks'
+import Home from './pages/Home'
 
 function App() {
-  let [task, setTask] = useState<Task>()
-
-  interface Task {
-    name: string,
-    description?: string,
-    duration?: number,
-    upvotes?: number
-  }
-
-  const fetchTask = async () => {
-    await fetch('http://localhost:5000/api/tasks').
-      then(response => response.json()).
-      then(data => setTask(data[0]))
-  } 
-
-  useEffect(() => {
-    fetchTask().catch(console.error)
-  }, [])
-
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <div className="Logo-container">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-      </header> */}
-      <div className="task">
-        <span className="task_name">{task?.name}</span>
-        <span className="task_description">{task?.description}</span>
-        <button className="task_search" onClick = {fetchTask}>
-          Find a task
-        </button>
-      </div>
+    <div className="app">
+      <header className="app-header">
+        <title>TaskJar</title>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />}/>  
+        <Route path="/tasks" element={<Tasks />}/>
+      </Routes>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import {Task} from '../types/types'
+
+export default function Tasks() {
+    let [tasks, setTasks] = useState<Task[]>()
+    let [userTasks, setUserTasks] = useState<Task[]>()
+
+    useEffect(() => {
+        const fetchAllTasks = async () => {
+            await fetch('http://localhost:5000/api/tasks/all').
+                then(response => response.json()).
+                then(data => setTasks(data))
+        } 
+
+        // const fetchUserTasks = async () => {
+        //     await fetch('http://localhost:5000/api/user/me', {
+        //         method: 'GET',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //             // 'Authorization': `Bearer ${token.jwt}`
+        //             },
+        //         }).
+        //         then(response => response.json()).
+        //         then(data => setUserTasks(data))
+        // }
+        
+        fetchAllTasks().catch(console.error)
+
+        // if (token) {
+        //     fetchUserTasks().catch(console.error)
+        // }
+        }, []
+    )
+
+    return <div>
+        {tasks?.map(task => <p>
+            {task.name}
+        </p>)}
+    </div>
+} 
